10cf6ef7f52749eed2b1bb4e2930d6ad73c66cfc
Melissa Prax <melissaprax@Melissas-MacBook-Pro.local>
2018-03-12 21:28:13 -0500

    mon at 9:30

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//page load. 
+
+// when user presses start, open new page. 
+//how to show new screen. replace div, ??
+
+//once new page opens, timer begins 
+//end game when done button is clicked
+//record and compare
+//collect click for each answer 
+//compare click to correct answer 
+//if correct, record
+//if incorrect, record as incorrect
+//after final click, show results page 
